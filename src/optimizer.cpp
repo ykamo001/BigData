4f7a135a877b9181906871d6fcfc197f73de4e67
ykamo001 <yash.kamothi@gmail.com>
2018-01-12 20:42:43 -0800

    updated functionality and made code faster

--- src/optimizer.cpp
+++ src/optimizer.cpp
@@ -9,6 +9,7 @@
 #include <sstream>
 #include <algorithm>
 #include <iterator>
+#include <map>
 
 using namespace std;
 
@@ -23,50 +24,32 @@ int k_fold_size(int matrixSize)		//this function will determine the size of K fo
 	return k;
 }
 
-void fix(vector<vector<double> > &neighbors, double dist, double qual)		//will be used to sort the top nearest neighbors
+void fix(map<double, double> &neighbors, double dist, double qual)	//will be used to sort the top nearest neighbors
 {
-	bool done = false;
-	int i = 0;
-	vector<double> temp;
-	vector<double> temp2;
-	double nDist = dist;
-	double nQual = qual;
-	while(!done) {	//keep doing so until we manually tell it to stop
-		temp = neighbors.at(i);
-		if(dist < temp.at(1)) {		//if we find that the new distance we found is smaller than the ones we already have
-			for(int j = i; j < neighbors.size(); ++j)	{	//then we simply insert it into the proper location
-				temp = neighbors.at(j);
-				temp2 = temp;
-				temp.at(0) = nQual;
-				temp.at(1) = nDist;
-				neighbors.at(j) = temp;
-				nDist = temp2.at(1);
-				nQual = temp2.at(0);
+	if(neighbors.size() > 2){
+		map<double, double>::reverse_iterator rit = neighbors.rbegin();
+		if(dist < rit->first){
+			while(neighbors.insert(pair<double, double>(dist, qual)).second == false){
+				dist += 0.00001;
 			}
-			done = true;
+			neighbors.erase(neighbors.rbegin()->first);
 		}
-		else {	//if we do not find any, stop looking
-			if(i+1 < neighbors.size()){
-				++i;
-			}
-			else{
-				done = true;
-			}
-		}
+	}
+	else{
+		neighbors.insert(pair<double, double>(dist, qual));
 	}
 }
 
-void validate(double qual, vector<vector<double> > neighbors, double &correct, double &tested)	//will be used to check
-{								//what the nearest neighbors think the point is
-	vector<double> temp;			//and if it is correct, we note that
+void validate(double qual, map<double, double> &neighbors, double &correct, double &tested)
+{ //will be used to check what the nearest neighbors think the point is
+	map<double, double>::iterator itr;
 	int pred = 0;
-	for(int i = 0; i < neighbors.size(); ++i) { //check how many neighbors think the point is what the actual point is
-		temp = neighbors.at(i);
-		if(qual == temp.at(0)) {
+	for(itr = neighbors.begin(); itr != neighbors.end(); itr++) {
+		if(qual == itr->second) {
 			pred++;
 		}
 	}
-	if(pred > 1) {		//if the majority of the neighbors think he is his actual qualifier, mark this as correct guess
+	if(pred > 1) {	//if the majority of the neighbors think he is his actual qualifier, mark this as correct guess
 		correct++;
 	}
 	tested++;
@@ -74,36 +57,25 @@ void validate(double qual, vector<vector<double> > neighbors, double &correct, d
 
 void k_fold(vector<vector<double> > &hold, vector<vector<double> > &test, double &correct, double &tested)
 {	//used to conduct k-fold and nearest neighbor algorithm
-	vector<double> currentTest;
-	vector<double> currentHold;
-	double dist = 0.0;
-	double temp = 0.0;
-	vector<double> distTemp;
-	double minDist = 100.0;
-	double minQual = 100.0;
+	vector<double> currentTest, currentHold;
+	double dist = 0.0, temp = 0.0, minDist = 100.0, minQual = 100.0;
 	for(int i = 0; i < test.size(); ++i) { //this is where we will conduct the nearest neighbor algorithm 
 		currentTest = test.at(i);
 		for(int j = 0; j < hold.size(); ++j) {
 			currentHold = hold.at(j);
-			for(int k = 1; k < currentTest.size(); ++k)	{
-				//use distance formula, eucledian like
-				//square root of (x1-x2)^2 + (y1-y2)^2 + ....
+			for(int k = 1; k < currentTest.size(); ++k)	{ //use distance formula, eucledian like square root of (x1-x2)^2 + (y1-y2)^2 + ....
 				temp = currentTest.at(k) - currentHold.at(k);
-				temp = temp*temp;
-				distTemp.push_back(temp);
+				temp *= temp;
+				dist += temp;
 			}
-			for(int l = 0; l < distTemp.size(); ++l) {
-				dist += distTemp.at(l);
-			}
-			distTemp.clear();
 			dist = sqrt(dist);
-			if(dist < minDist)	{		//keep track of what the nearest neighbor is
+			if(dist < minDist)	{	//keep track of what classification the nearest neighbor is
 				minDist = dist;
 				minQual = currentHold.at(0);
 			}
 			dist = 0.0;
 		}
-		if(minQual == currentTest.at(0)) {	//if the nearest neighbor thinks the point is actually what the point is, then correct
+		if(minQual == currentTest.at(0)) {	//if the nearest neighbor thinks the point is what our data says, then it is correct
 			correct++;
 		}
 		tested++;
@@ -111,24 +83,16 @@ void k_fold(vector<vector<double> > &hold, vector<vector<double> > &test, double
 		minQual = 100.0;
 		dist = 0.0;
 		temp = 0.0;
-		distTemp.clear();
 	}
 }
 
 void k_fold_neigh(vector<vector<double> > &hold, vector<vector<double> > &test, double &correct, double &tested)
 {		//this is the custom algorithm, asks a majority of what the neighbors think
-	vector<double> currentTest;
-	vector<double> currentHold;
-	double dist = 0.0;
-	double temp = 0.0;
+	vector<double> currentTest, currentHold;
+	double dist = 0.0, temp = 0.0;
 	vector<double> distTemp;
-	vector<vector<double> > neighbors;
-	distTemp.push_back(10.0);		//initialize the neighbors to junk value
-	distTemp.push_back(100.0);
-	for(int q = 0; q < 3; ++q)	{	//we will ask 3 neighbors
-		neighbors.push_back(distTemp);
-	}
-	distTemp.clear();
+	map<double, double> neighbors;
+
 	for(int i = 0; i < test.size(); ++i) {	//this is where we will conduct the nearest neighbor algorithm 
 		currentTest = test.at(i);
 		for(int j = 0; j < hold.size(); ++j) {
@@ -136,27 +100,17 @@ void k_fold_neigh(vector<vector<double> > &hold, vector<vector<double> > &test, 
 			for(int k = 1; k < currentTest.size(); ++k) { //use distance algorithm, just like forward search
 				temp = currentTest.at(k) - currentHold.at(k);
 				temp = temp*temp;
-				distTemp.push_back(temp);
+				dist += temp;
 			}
-			for(int l = 0; l < distTemp.size(); ++l) {
-				dist += distTemp.at(l);
-			}
-			distTemp.clear();
 			dist = sqrt(dist);
 			fix(neighbors, dist, currentHold.at(0));	//hold all the nearest neighbors
 			dist = 0.0;
+			temp = 0.0;
 		}
 		dist = 0.0;
 		temp = 0.0;
-		distTemp.clear();
 		validate(currentTest.at(0), neighbors, correct, tested);	//check for what neighbors think
 		neighbors.clear();
-		distTemp.push_back(10.0);		
-		distTemp.push_back(100.0);
-		for(int q = 0; q < 3; ++q)	{
-			neighbors.push_back(distTemp);
-		}
-		distTemp.clear();
 	}
 }
 
@@ -164,21 +118,16 @@ double cross_validation(vector<vector<double> > matrix, char opt)		//sets up the
 {
 	int k = k_fold_size(matrix.size());
 	int iter = matrix.size() / k;
-	int start = 0;
-	int stop = iter - 1;
-	double correct = 0.0;
-	double tested = 0.0;
-	vector<vector<double> > hold;
-	vector<vector<double> > test;
-	vector<double> temp;
+	int start = 0, stop = iter - 1;
+	double correct = 0.0, tested = 0.0;
+	vector<vector<double> > hold, test;
 	for(int i = 0; i < k; ++i) {			//must do k-fold k times
 		for(int j = 0; j < matrix.size(); ++j)	{	//get the correct section to test
-			temp = matrix.at(j);
-			if((j >= start) && (j <= stop)){
-				test.push_back(temp);
+			if((j >= start) && (j <= stop)) {
+				test.push_back(matrix.at(j));
 			}
-			else{
-				hold.push_back(temp);
+			else {
+				hold.push_back(matrix.at(j));
 			}
 		}
 		if(opt == '1')	{	//for forward search and backward search
@@ -187,7 +136,6 @@ double cross_validation(vector<vector<double> > matrix, char opt)		//sets up the
 		else {	//only for custom search
 			k_fold_neigh(hold, test, correct, tested);
 		}
-		double perc = correct/tested;
 		hold.clear();
 		test.clear();
 		start = stop + 1;
@@ -240,7 +188,7 @@ bool checker(vector<int> index_features, int index)		//checks for if the current
 }
 
 void adder(vector<vector<double> > &deter_list, const vector<vector<double> > &matrix, int j)
-{			//adds the proper features to our searching matrix
+{	//adds the proper features to our searching matrix
 	vector<double> temp;
 	vector<double> temp2;
 	for(int i = 0; i < matrix.size(); ++i) {
@@ -277,7 +225,7 @@ void remover(vector<vector<double> > &deter_list,  vector<vector<double> > matri
 }
 
 void forward_search(vector<vector<double> > &matrix, vector<int> &best_features, double &best_acc, char opt)
-{			//the forward search
+{	//the forward search
 	vector<int> index_features;
 	vector<vector<double> > current_list;
 	vector<vector<double> > deter_list;
@@ -297,7 +245,7 @@ void forward_search(vector<vector<double> > &matrix, vector<int> &best_features,
 	bool used = false;
 	for(int i = 1; i < stop; ++i) {		//we iterate as many times as how many features we have
 		cout << "Iteration: " << i << ". Searching over features: ";
-		for(int z = 0; z < index_features.size(); ++z) {
+		for(int z = 0; z > index_features.size(); z++) {
 			cout << index_features.at(z) << " ";
 		}
 		cout << endl;
